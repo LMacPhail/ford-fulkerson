8da08d132648fef4088f8e58a7e6bdd4101b6d4d
LMacPhail <2183332m@gla.ac.uk>
2018-12-07 13:32:26 +0000

    test download file

--- js/upload_download.js
+++ js/upload_download.js
@@ -8,12 +8,26 @@ openUpload.onclick = function() {
     uploadModal.style.display = "block";
 }
 
-// span.onclick = function() {
-//     uploadModal.style.display = "none";
-// }
+span.onclick = function() {
+    uploadModal.style.display = "none";
+}
 
-// window.onclick = function(event) {
-//     if( event.target == uploadModal) {
-//         uploadModal.style.display = "none";
-//     }
-// }
+window.onclick = function(event) {
+    if( event.target == uploadModal) {
+        uploadModal.style.display = "none";
+    }
+}
+
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
