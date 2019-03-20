488a2da64628237e79bbcaed4d0850cf8c307e05
LMacPhail <2183332m@gla.ac.uk>
2019-03-20 13:11:00 +0000

    added min_cut test back too

--- js/tests/main_test.js
+++ js/tests/main_test.js
@@ -16,3 +16,42 @@ var options = {
 // }
 N = 30;
 testFF(7000);
+
+
+function testFF(iter){
+  // N = 26;
+  var FFMaxFlow, cutMaxFlow, cut = [], incorrect = 0, i;
+  var notBroken = true;
+  var topEdgeText, resEdgeText;
+  for(i = 0; i < iter; i++){
+    generateRandomGraph();
+    FFMaxFlow = fordFulkerson();
+    cut = findMinimumCut();
+    // $('body').append('<p>Ford Fulkerson maximum flow = ' + FFMaxFlow + '</p>');
+    // $('body').append('<p>Minimum cut found = ' + cut + '</p>');
+    cutMaxFlow = removeEdgesInCut(cut);
+    // $('body').append('<p>Minimum cut flow = ' + cutMaxFlow + '</p>');
+    if(!(cutMaxFlow == FFMaxFlow)) {
+      $('body').append('<p>Minimum cut flow = ' + cutMaxFlow + '</p>');
+      $('body').append('<p>Ford Fulkerson maximum flow = ' + FFMaxFlow + '</p>');
+
+      console.log(algTopEdges);
+      console.log(algResEdges);
+      console.log(resAdjMatrix);
+      // notBroken = false;
+      incorrect++;
+    }
+    // notBroken = false;
+  }
+  $('body').append('<p>Percentage incorrect = ' + (incorrect / iter)*100 + '</p>');
+}
+
+function removeEdgesInCut(cut) {
+  var cutFlow = 0;
+  for(var i = 0; i < cut.length; i++) {
+    var edge = algTopEdges.get(cut[i]);
+    cutFlow += parseInt(getCapacity(edge.label));
+    // algTopEdges.remove(cut[i]);
+  }
+  return cutFlow;
+}
